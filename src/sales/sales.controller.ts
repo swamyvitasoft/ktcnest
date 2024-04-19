16ca7646331d34c8612b217e8ca2aa36bfddb3c3
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-04-19 15:39:07 +0530

    sales controller and service changed

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,159 +1,193 @@
-import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  UseGuards,
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { SalesDto } from './dto/sales.dto';
 import { Sales } from './schema/sales.schema';
 import { verifyToken } from '../jwt/verifyToken';
 import { validate } from 'class-validator';
 
-
 @Controller('sales')
 export class SalesController {
-  
-    constructor(private readonly salesService: SalesService) {}
+  constructor(private readonly salesService: SalesService) {}
 
- @Post()
- @UseGuards(verifyToken)
- async create(@Body() salesDto: SalesDto): Promise<any> {
-    try{
-      const newSale = await this.salesService.create(salesDto);
-      return{ newSale }
-    }catch(error){
-      throw new HttpException({ error:'sale adding failed' },HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-   
- };
- 
- @Get('')
- @UseGuards(verifyToken)
-    async findAll(): Promise<any> {
-      try{
-        const allSales = await this.salesService.findAll();
-        return { allSales }
-      }catch(error){
-        throw new HttpException({ error:'sales not found' },HttpStatus.INTERNAL_SERVER_ERROR)
+  @Post()
+  @UseGuards(verifyToken)
+  async create(@Body() salesDto: SalesDto): Promise<Sales> {
+    try {
+      const errors = await validate(salesDto);
+      if (errors.length > 0) {
+        throw new BadRequestException('Validation failed.');
       }
-       
-    };
-
-@Get('totalsales')
- @UseGuards(verifyToken)
-  async totalSales(): Promise<Sales[]> {
-   return this.salesService.totalSales();
-   };
-
- @Get('topsales')
- @UseGuards(verifyToken)
-    async getTopSales(): Promise<any> {
-        return this.salesService.getTopSales();
-    };
-
- @Get('customers')
- @UseGuards(verifyToken)
-    async getCustomers(): Promise<any> {
-        return this.salesService.getCustomers();
-    };
-
- @Get('yearly') 
- @UseGuards(verifyToken)
-    async getYearly(): Promise<any> {
-        return this.salesService.getYearly();
-    };
-
-  @Post('monthly') 
-  @UseGuards(verifyToken)
-  async getMonthly(): Promise<any> {
-      return this.salesService.getMonthly();
+      return await this.salesService.create(salesDto);
+    } catch (error) {
+      throw new InternalServerErrorException('sale adding failed');
+    }
   }
 
-  @Post('daily') 
+  @Get()
   @UseGuards(verifyToken)
-  async getDaily(): Promise<any> {
-      return this.salesService.getDaily();
+  async findAll(): Promise<Sales[]> {
+    try {
+      return await this.salesService.findAll();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
   }
 
-  @Post('exports') 
+  @Put(':id')
   @UseGuards(verifyToken)
-  async getExport(@Body() body): Promise<any> {
+  async update(
+    @Param('id') id: string,
+    @Body() salesDto: SalesDto,
+  ): Promise<Sales> {
+    try {
+      const errors = await validate(salesDto);
+      if (errors.length > 0) {
+        throw new BadRequestException('Validation failed.');
+      }
+      const Sale = await this.salesService.update(id, salesDto);
+      if (!Sale) {
+        throw new NotFoundException('Sale not found.');
+      }
+      return Sale;
+    } catch (error) {
+      if (error.status === 404) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Could not update Sale.');
+    }
+  }
+
+  @Delete(':id')
+  @UseGuards(verifyToken)
+  async remove(@Param('id') id: string): Promise<any> {
+    try {
+      const Sale = await this.salesService.delete(id);
+      if (!Sale) {
+        throw new NotFoundException('Sale not found.');
+      }
+      return Sale;
+    } catch (error) {
+      if (error.status === 404) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Could not delete Sale.');
+    }
+  }
+
+  @Get('totalsales')
+  @UseGuards(verifyToken)
+  async totalSales(): Promise<any> {
+    try {
+      return this.salesService.totalsales();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
+  }
+
+  @Get('topsales')
+  @UseGuards(verifyToken)
+  async topSales(): Promise<any> {
+    try {
+      return this.salesService.topsales();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
+  }
+
+  @Post('daysales')
+  @UseGuards(verifyToken)
+  async daySales(): Promise<any> {
+    try {
+      return this.salesService.daysales();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
+  }
+
+  @Post('monthsales')
+  @UseGuards(verifyToken)
+  async monthSales(): Promise<any> {
+    try {
+      return this.salesService.monthsales();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
+  }
+
+  @Get('yearsales')
+  @UseGuards(verifyToken)
+  async yearSales(): Promise<any> {
+    try {
+      return this.salesService.yearsales();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
+    }
+  }
+
+  @Post('exports')
+  @UseGuards(verifyToken)
+  async Exports(@Body() body): Promise<any> {
     try {
       const { datefrom, dateto } = body;
       if (!datefrom || !dateto) {
-          throw new HttpException('Date parameters are missing', HttpStatus.BAD_REQUEST);
+        throw new BadRequestException('Date parameters are missing.');
       }
-
-        const fromDate = new Date(datefrom);
-        const toDate = new Date(dateto);
-
-        if (toDate <= fromDate) {
-            throw new HttpException('End date must be after the start date', HttpStatus.BAD_REQUEST);
-        }
-
+      const fromDate = new Date(datefrom);
+      const toDate = new Date(dateto);
+      if (toDate <= fromDate) {
+        throw new BadRequestException('End date must be after the start date.');
+      }
       const currentDate = new Date();
-        const Dateto = new Date(dateto);
-
-        const currentDateWithoutTime = new Date(currentDate.toDateString());
-        const toDateWithoutTime = new Date(Dateto.toDateString());
-
-        if (toDateWithoutTime > currentDateWithoutTime) {
-            throw new HttpException('End date must be today or before today', HttpStatus.BAD_REQUEST);
-        }
-
-      return await this.salesService.getExport(datefrom, dateto);
-  } catch (error) {
-      if (error instanceof HttpException) {
-          throw error; 
-      } else {
-          throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+      const Dateto = new Date(dateto);
+      const currentDateWithoutTime = new Date(currentDate.toDateString());
+      const toDateWithoutTime = new Date(Dateto.toDateString());
+      if (toDateWithoutTime > currentDateWithoutTime) {
+        throw new BadRequestException('End date must be today or before today');
       }
-  }
+      return await this.salesService.exports(datefrom, dateto);
+    } catch (error) {
+      throw new InternalServerErrorException('Internal server error');
+    }
   }
 
- @Get(':id')
- @UseGuards(verifyToken)
-  async findOne(@Param('id') id: string): Promise<any> {
-    try{
-      const sale = await this.salesService.findOne(id);
-      return { sale }
-    }catch(error){
-      throw new HttpException({ error:'sale not found'},HttpStatus.INTERNAL_SERVER_ERROR) 
-    }
-   
-  };
-
-  @Put('/update/:id')
+  @Get('customers')
   @UseGuards(verifyToken)
-  async update(@Param('id') id: string, @Body() salesDto: SalesDto): Promise<any> {
-    try{
-      const errors = await validate(salesDto);
-      if (errors.length > 0) {
-        throw new HttpException({ error: 'Validation failed', details: errors }, HttpStatus.BAD_REQUEST);
-      }
-
-      const updateSale = await this.salesService.update(id, salesDto);
-      if (!updateSale) {
-        throw new HttpException({ error: 'Sale not found' }, HttpStatus.NOT_FOUND);
-      }
-      return { updateSale }
-    }catch(error){
-      throw new HttpException({ error:'failed to update sale'},HttpStatus.INTERNAL_SERVER_ERROR)
-    }    
-  };
-  
-
-  @Delete('/delete/:id')
-  @UseGuards(verifyToken)
-  async remove(@Param('id') id: string): Promise<any> {
-    try{
-      const deletedSale = await this.salesService.remove(id);
-      if (!deletedSale) {
-        throw new HttpException({ error: 'Sale not found' }, HttpStatus.NOT_FOUND);
-      }
-      return {deletedSale}
-    }catch(error){
-      throw new HttpException({ error:'failed to delete sale'},HttpStatus.INTERNAL_SERVER_ERROR)
+  async Customers(): Promise<any> {
+    try {
+      return this.salesService.customers();
+    } catch (error) {
+      throw new InternalServerErrorException('sales not found.');
     }
-    
-  };
+  }
 
-  
- 
-};
+  @Get(':id')
+  @UseGuards(verifyToken)
+  async findOne(@Param('id') id: string): Promise<Sales> {
+    try {
+      const Sale = await this.salesService.findOne(id);
+      if (!Sale) {
+        throw new NotFoundException('Sale not found.');
+      }
+      return Sale;
+    } catch (error) {
+      if (error.status === 404) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Could not fetch Sale.');
+    }
+  }
+}
--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,301 +1,259 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { SalesDto } from './dto/sales.dto'; 
+import { SalesDto } from './dto/sales.dto';
 import { Sales } from './schema/sales.schema';
 
 @Injectable()
 export class SalesService {
+  constructor(@InjectModel('Sales') private salesModel: Model<Sales>) {}
 
-    constructor(@InjectModel('Sales') private salesModel: Model<Sales>) {} 
+  async create(salesDto: SalesDto): Promise<Sales> {
+    const createdSale = new this.salesModel(salesDto);
+    return createdSale.save();
+  }
 
-    async create(salesDto: SalesDto): Promise<Sales> {
-        const createdSale = new this.salesModel(salesDto);
-        return createdSale.save();
-      }
+  async findAll(): Promise<Sales[]> {
+    return this.salesModel.find().exec();
+  }
 
-      async findAll(): Promise<Sales[]> {
-        return this.salesModel.find().exec();
-      }
-    
-      async findOne(id: string): Promise<Sales> {
-        return this.salesModel.findById(id).exec();
-      }
-      
-      async update(id: string, salesDto: SalesDto): Promise<Sales> {
-        return this.salesModel.findByIdAndUpdate(id, salesDto, { new: true }).exec();
-      }
-    
-      async remove(id: string,): Promise<any> {
-       const delatedSale = await this.salesModel.findByIdAndDelete(id).exec();
-       return delatedSale
-      }
+  async findOne(id: string): Promise<Sales> {
+    return this.salesModel.findById(id).exec();
+  }
 
-      async totalSales(): Promise<any> {
-        try {
-            const totalSales = await this.salesModel.aggregate([
-                {
-                    $match: {
-                        status: "active",
-                    },
-                },
-                {
-                    $lookup: {
-                        from: "items",
-                        localField: "itemId",
-                        foreignField: "_id",
-                        as: "items",
-                    },
-              },
-             
-        ]);
-        return totalSales;
-    } catch (err) {
-        throw new Error('Sales not found');
-    }
+  async update(id: string, salesDto: SalesDto): Promise<Sales> {
+    return this.salesModel
+      .findByIdAndUpdate(id, salesDto, { new: true })
+      .exec();
+  }
+
+  async delete(id: string): Promise<Sales> {
+    return await this.salesModel.findByIdAndDelete(id).exec();
+  }
+
+  async totalsales(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          status: 'active',
+        },
+      },
+      {
+        $lookup: {
+          from: 'items',
+          localField: 'itemId',
+          foreignField: '_id',
+          as: 'items',
+        },
+      },
+    ]);
+  }
+
+  async topsales(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          status: 'active',
+        },
+      },
+      {
+        $lookup: {
+          from: 'items',
+          localField: 'itemId',
+          foreignField: '_id',
+          as: 'items',
+        },
+      },
+      {
+        $group: {
+          _id: '$itemId',
+          count: {
+            $sum: 1,
+          },
+          totalAdvanceAmount: {
+            $sum: '$advanceamount',
+          },
+          totalEstimatedAmount: {
+            $sum: '$estimatedamount',
+          },
+          totalBalanceAmount: {
+            $sum: '$balaceamount',
+          },
+        },
+      },
+      {
+        $sort: {
+          count: -1,
+        },
+      },
+      {
+        $lookup: {
+          from: 'items',
+          localField: '_id',
+          foreignField: '_id',
+          as: 'items',
+        },
+      },
+    ]);
+  }
+
+  async daysales(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: new Date('Thu, 01 Apr 2024 00:00:00 GMT'),
+            $lt: new Date('Fri, 01 May 2024 00:00:00 GMT'),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $month: '$createdAt',
+          },
+          createdAt: {
+            $first: '$createdAt',
+          },
+          totalEstimatedAmount: {
+            $sum: '$estimatedamount',
+          },
+          totalAdvanceAmount: {
+            $sum: '$advanceamount',
+          },
+          totalBalanceAmount: {
+            $sum: '$balaceamount',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          month: '$_id',
+          createdAt: 1,
+          totalEstimatedAmount: 1,
+          totalAdvanceAmount: 1,
+          totalBalanceAmount: 1,
+        },
+      },
+    ]);
+  }
+
+  async monthsales(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: new Date('Mon, 01 Jan 2024 00:00:00 GMT'),
+            $lt: new Date('Wed, 01 Jan 2025 00:00:00 GMT'),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $month: '$createdAt',
+          },
+          createdAt: {
+            $first: '$createdAt',
+          },
+          estimatedTotalAmount: {
+            $sum: '$estimatedamount',
+          },
+          advanceTotalAmount: {
+            $sum: '$advanceamount',
+          },
+          balanceTotalAmount: {
+            $sum: '$balaceamount',
+          },
+        },
+      },
+    ]);
+  }
+
+  async yearsales(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          status: 'active',
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $year: '$createdAt',
+          },
+          count: {
+            $sum: 1,
+          },
+          totalAdvanceAmount: {
+            $sum: '$advanceamount',
+          },
+          totalEstimatedAmount: {
+            $sum: '$estimatedamount',
+          },
+          totalBalanceAmount: {
+            $sum: '$balaceamount',
+          },
+        },
+      },
+    ]);
+  }
+
+  async exports(datefrom: Date, dateto: Date): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $lookup: {
+          from: 'items',
+          localField: 'itemId',
+          foreignField: '_id',
+          as: 'items',
+        },
+      },
+      {
+        $sort: {
+          createdAt: -1,
+        },
+      },
+      {
+        $match: {
+          createdAt: {
+            $gte: new Date(datefrom),
+            $lt: new Date(dateto),
+          },
+          status: 'active',
+        },
+      },
+    ]);
+  }
+
+  async customers(): Promise<any> {
+    return await this.salesModel.aggregate([
+      {
+        $match: {
+          status: 'active',
+        },
+      },
+      {
+        $group: {
+          _id: '$mobileno',
+          count: {
+            $sum: 1,
+          },
+          fullname: {
+            $first: '$fullname',
+          },
+          totalAdvanceAmount: {
+            $sum: '$advanceamount',
+          },
+          totalEstimatedAmount: {
+            $sum: '$estimatedamount',
+          },
+          totalBalanceAmount: {
+            $sum: '$balaceamount',
+          },
+        },
+      },
+    ]);
+  }
 }
-
-
-async getTopSales(): Promise<any> {
-    try {
-        const topSales = await this.salesModel.aggregate([
-            {
-                $match: {
-                    status: "active",
-                },
-            },
-            {
-                $lookup: {
-                    from: "items",
-                    localField: "itemId",
-                    foreignField: "_id",
-                    as: "items",
-                },
-            },
-            {
-                $group: {
-                    _id: "$itemId",
-                    count: {
-                        $sum: 1,
-                    },
-                    totalAdvanceAmount: {
-                        $sum: "$advanceamount",
-                    },
-                    totalEstimatedAmount: {
-                        $sum: "$estimatedamount",
-                    },
-                    totalBalanceAmount: {
-                        $sum: "$balaceamount",
-                    },
-                },
-            },
-            {
-                $sort: {
-                    count: -1,
-                },
-            },
-            {
-                $lookup: {
-                    from: "items",
-                    localField: "_id",
-                    foreignField: "_id",
-                    as: "items",
-                },
-            },
-        ]);
-        return topSales;
-    } catch (err) {
-        throw new Error('Top sales not found');
-    }
-}
-
-async getCustomers(): Promise<any> {
-    try {
-        const allCustomers = await this.salesModel.aggregate([
-            {
-                $match: {
-                    status: "active",
-                },
-            },
-            {
-                $group: {
-                    _id: "$mobileno",
-                    count: {
-                        $sum: 1,
-                    },
-                    fullname: {
-                        $first: "$fullname",
-                    },
-                    totalAdvanceAmount: {
-                        $sum: "$advanceamount",
-                    },
-                    totalEstimatedAmount: {
-                        $sum: "$estimatedamount",
-                    },
-                    totalBalanceAmount: {
-                        $sum: "$balaceamount",
-                    },
-                },
-            },
-        ]);
-        return allCustomers;
-    } catch (err) {
-        throw new Error('Customers not found');
-    }
-};
-
-
-async getYearly(): Promise<any> {
-    try {
-        const yearlyData = await this.salesModel.aggregate([
-            {
-                $match: {
-                    status: "active",
-                },
-            },
-            {
-                $group: {
-                    _id: {
-                        $year: "$createdAt",
-                    },
-                    count: {
-                        $sum: 1,
-                    },
-                    totalAdvanceAmount: {
-                        $sum: "$advanceamount",
-                    },
-                    totalEstimatedAmount: {
-                        $sum: "$estimatedamount",
-                    },
-                    totalBalanceAmount: {
-                        $sum: "$balaceamount",
-                    },
-                },
-            },
-        ]);
-        return yearlyData;
-    } catch (err) {
-        throw new Error('Yearly data not found');
-    }
-};
-
-async getMonthly(): Promise<any> {
-    try {
-        const monthlyData = await this.salesModel.aggregate([
-            {
-                $match: {
-                    createdAt: {
-                        $gte: new Date("Mon, 01 Jan 2024 00:00:00 GMT"),
-                        $lt: new Date("Wed, 01 Jan 2025 00:00:00 GMT"),
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: {
-                        $month: "$createdAt",
-                    },
-                    createdAt: {
-                        $first: "$createdAt",
-                    },
-                    estimatedTotalAmount: {
-                        $sum: "$estimatedamount",
-                    },
-                    advanceTotalAmount: {
-                        $sum: "$advanceamount",
-                    },
-                    balanceTotalAmount: {
-                        $sum: "$balaceamount",
-                    },
-                },
-            },
-        ]);
-        return monthlyData;
-    } catch (err) {
-        throw new Error('Monthly data not found');
-    }
-};
-
-async getDaily(): Promise<any> {
-    try {
-        const dailyData = await this.salesModel.aggregate([
-            {
-                $match: {
-                    createdAt: {
-                        $gte: new Date("Thu, 01 Apr 2024 00:00:00 GMT"),
-                        $lt: new Date("Fri, 01 May 2024 00:00:00 GMT"),
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: {
-                        $month: "$createdAt",
-                    },
-                    createdAt: {
-                        $first: "$createdAt",
-                    },
-                    totalEstimatedAmount: {
-                        $sum: "$estimatedamount",
-                    },
-                    totalAdvanceAmount: {
-                        $sum: "$advanceamount",
-                    },
-                    totalBalanceAmount: {
-                        $sum: "$balaceamount",
-                    },
-                },
-            },
-            {
-                $project: {
-                    _id: 0,
-                    month: "$_id",
-                    createdAt: 1,
-                    totalEstimatedAmount: 1,
-                    totalAdvanceAmount: 1,
-                    totalBalanceAmount: 1,
-                },
-            },
-        ]);
-        return dailyData;
-    } catch (err) {
-        throw new Error('Daily data not found');
-    }
-};
-
-
-async getExport(datefrom: Date, dateto: Date): Promise<any> {
-    try {
-        const allSales = await this.salesModel.aggregate([
-            {
-                $lookup: {
-                    from: "items",
-                    localField: "itemId",
-                    foreignField: "_id",
-                    as: "items",
-                },
-            },
-            {
-                $sort: {
-                    createdAt: -1,
-                },
-            },
-            {
-                $match: {
-                    createdAt: {
-                        $gte: new Date(datefrom),
-                        $lt: new Date(dateto),
-                    },
-                    status: "active",
-                },
-            },
-        ]);
-        return allSales;
-    } catch (err) {
-        throw new Error('Export data not found');
-    }
-}
-
-};
-
-
