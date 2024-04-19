4e64717ed371eb8eaaf207627925535e827e924d
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-04-19 17:45:05 +0530

    mobile validations

--- src/admin/dto/admin.dto.ts
+++ src/admin/dto/admin.dto.ts
@@ -1,7 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
 export class AdminDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(10, { message: 'mobileno must be 10 numders only' })
+  @MaxLength(10, { message: 'mobileno must be 10 numders only' })
   mobileno: string;
 
   @IsNotEmpty()
